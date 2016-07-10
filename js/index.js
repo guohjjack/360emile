f710ee72f0a2b9fb134d04007c703312d5ee4894
guohjjack <guohjjack@sina.com>
2016-07-11 00:06:04 +0800

    360emile

---
+++ js/index.js
@@ -0,0 +1,7 @@
+    var swiper = new Swiper('.fenshou', {
+        // pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30,
+        freeMode: true
+    });
